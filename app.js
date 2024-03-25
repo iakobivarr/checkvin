e507dcba3132d6f27080c5ba50debd2e3ee7b167
iakobivarr <unknown20415@gmail.com>
2024-03-26 00:52:51 +0400

    modified:   app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Replace 'YOUR_SITE_KEY' with your reCAPTCHA site key
-var siteKey = 'YOUR_SITE_KEY';
+var siteKey = '6LcYETIUAAAAAKz6T9MxMEllN8yw0ffsErIbAGS-';
 
 // Replace 'RECAPTCHA_RESPONSE_HERE' with the actual reCAPTCHA response
 var recaptchaResponse = 'RECAPTCHA_RESPONSE_HERE';
@@ -10,16 +10,17 @@ var userIpAddress = 'USER_IP_ADDRESS_HERE';
 // Make a POST request to the reCAPTCHA verification endpoint
 fetch('https://www.google.com/recaptcha/api/siteverify', {
     method: 'POST',
+    mode: 'no-cors', // Set mode to 'no-cors'
     headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
     },
     body: 'secret=' + siteKey + '&response=' + recaptchaResponse + '&remoteip=' + userIpAddress
 })
-.then(response => response.json())
-.then(data => {
-    // Log the response to the console
-    console.log('reCAPTCHA verification response:', data);
+.then(response => {
+    // Handle success (note: you won't be able to access response body)
+    console.log('Success:', response);
 })
 .catch(error => {
+    // Handle errors
     console.error('Error during reCAPTCHA verification:', error);
 });
